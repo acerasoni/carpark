86a48160a1ac51db1421ac206237dd16fa2236ce
Andrea Cerasoni <andreacerasoni@gmail.com>
2023-09-19 10:30:22 +0100

    Complete ParkingServiceTest

--- src/test/java/com/acerasoni/carpark/service/ExitServiceTest.java
+++ src/test/java/com/acerasoni/carpark/service/ExitServiceTest.java
@@ -1,6 +1,7 @@
 package com.acerasoni.carpark.service;
 
 import com.acerasoni.carpark.model.Car;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -11,33 +12,54 @@ import reactor.test.StepVerifier;
 @ExtendWith(MockitoExtension.class)
 class ExitServiceTest {
 
-    @Mock
-    private FormattingService formattingService;
+    private static final Car CAR_ONE = new Car(0L);
+    private static final Car CAR_TWO = new Car(1L);
+    private static final Car CAR_THREE = new Car(2L);
+
+    private ExitService exitService;
+    private Sinks.Many<Car> testCarPark;
+
+    @BeforeEach
+    void setUp(@Mock final FormattingService formattingService) {
+        testCarPark = Sinks.many().unicast().onBackpressureBuffer();
+        exitService = new ExitService(100, formattingService, testCarPark);
+    }
 
     @Test
-    void testGenerateExitQueue() {
-        final Sinks.Many<Car> testSink = Sinks.many().unicast().onBackpressureBuffer();
-        final Car carOne = new Car(0L);
-        final Car carTwo = new Car(1L);
-        final Car carThree = new Car(3L);
-        testSink.tryEmitNext(carOne);
-        testSink.tryEmitNext(carTwo);
-
-        final ExitService exitService = new ExitService(100, formattingService, testSink);
+    void generateExitQueue_whenCarsEmittedAsync_thenVerifyFluxPublisherWithEmittedCars() {
+        testCarPark.tryEmitNext(CAR_ONE);
+        testCarPark.tryEmitNext(CAR_TWO);
 
         final var stepVerifier = StepVerifier
                 .create(exitService.generateExitQueue());
 
         stepVerifier
                 .expectSubscription()
-                .expectNext(carOne)
-                .expectNext(carTwo);
+                .expectNext(CAR_ONE)
+                .expectNext(CAR_TWO);
 
-        testSink.tryEmitNext(carThree);
-        testSink.tryEmitComplete();
+        testCarPark.tryEmitNext(CAR_THREE);
+        testCarPark.tryEmitComplete();
 
         stepVerifier
-                .expectNext(carThree)
+                .expectNext(CAR_THREE)
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    void generateExitQueue_whenCarsEmittedSync_thenVerifyFluxPublisherWithEmittedCars() {
+        testCarPark.tryEmitNext(CAR_ONE);
+        testCarPark.tryEmitNext(CAR_TWO);
+        testCarPark.tryEmitNext(CAR_THREE);
+        testCarPark.tryEmitComplete();
+
+        StepVerifier
+                .create(exitService.generateExitQueue())
+                .expectSubscription()
+                .expectNext(CAR_ONE)
+                .expectNext(CAR_TWO)
+                .expectNext(CAR_THREE)
                 .expectComplete()
                 .verify();
     }
